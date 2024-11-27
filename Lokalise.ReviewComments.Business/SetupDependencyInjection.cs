a2a2b21d63656ae4ed7523e4627a62b0db3be31f
Theodor Risager <tri_exr@dawnhealth.com>
2024-11-27 11:07:15 +0100

    Compiles and Runs

--- Lokalise.ReviewComments.Business/SetupDependencyInjection.cs
+++ Lokalise.ReviewComments.Business/SetupDependencyInjection.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using Lokalise.ReviewComments.Business.Extenstions;
 using Lokalise.ReviewComments.Business.Interfaces;
+using Lokalise.ReviewComments.Business.Services;
 using Lokalise.ReviewComments.Business.Services.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,16 +18,28 @@ public static class SetupDependencyInjection
             throw new ArgumentNullException("LokaliseSettings");
         
         services.AddTransient<IApp, App>();
+        services.AddTransient<ICommandService, CommandService>();
+        services.AddTransient<IDataService, DataService>();
+        services.AddTransient<ILokaliseApiClient, LokaliseApiClient>();
+        services.AddTransient<ILokaliseCookieClient, LokaliseCookieClient>();
+        services.AddTransient<IUserInteractionService, UserInteractionService>();
+        
+        SetupHttp(services, lokaliseSettings);
+        return services;
+    }
+
+    private static void SetupHttp(IServiceCollection services, LokaliseSettings lokaliseSettings)
+    {
         services.AddHttpClient<ILokaliseApiClient, LokaliseApiClient>(client =>
         {
             client.DefaultRequestHeaders.Add("X-Api-Token", lokaliseSettings.ApiToken);
             client.DefaultRequestHeaders.Add("accept", "application/json");
-            client.DefaultRequestHeaders.Add("content-type", "application/json");
+            // client.DefaultRequestHeaders.Add("content-type", "application/json");
             client.BaseAddress = new Uri(lokaliseSettings.BaseApiUrl);
         });
         services.AddHttpClient<ILokaliseCookieClient, LokaliseCookieClient>(client =>
         {
-            client.DefaultRequestHeaders.Add("accept", "/*");
+            client.DefaultRequestHeaders.Add("accept", "*/*");
             client.DefaultRequestHeaders.Add("x-csrf-token", lokaliseSettings.XCsrfToken);
             client.BaseAddress = new Uri(lokaliseSettings.BaseCookieUrl);
         }).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
@@ -36,11 +49,10 @@ public static class SetupDependencyInjection
             {
                 foreach (var cookie in lokaliseSettings.Cookies)
                 {
-                    container.Add(new Uri(lokaliseSettings.AppDomain), 
+                    container.Add(new Uri(lokaliseSettings.BaseCookieUrl), 
                         new Cookie(cookie.Key, cookie.Value) { Domain = lokaliseSettings.AppDomain });
                 }
             })
         });
-        return services;
     }
 }
